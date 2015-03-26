31265e5c25144a2d535c1b6ea37203ef2bd15fe7
Виктор <vitya.port@gmail.com>
2015-03-26 19:20:30 +0200

    init

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require_once('project.php');
 require_once('controller.Add.php');
 require_once('controller.delete.php');
 require_once('controller.form.php');
+require_once('controller.index.php');
 
 
 $loader = new Twig_Loader_Filesystem('templates');
@@ -16,36 +17,23 @@ $twig = new Twig_Environment($loader,
 );
 $twig->addExtension(new Twig_Extension_Debug());
 
-$payments =getPayments();
-$sum=summ('summa','zvit');
+
 $param=array('','');
-echo $twig->render('index.html',array('payments' => $payments,'sum'=>$sum));
+
 if (array_key_exists('go', $_REQUEST))
-    {
-    $go=$_REQUEST['go'];
+{
+    $go = $_REQUEST['go'];
 
-        $param=array($twig,'');
-        echo call_user_func_array ($go,$param);
-
-
-    }
-
-
-//call_user_func_array ($func,$param);
-/*switch ($go) {
-    case '':
-        echo $twig->render('index.html',array('payments' => $payments));
-
-        break;
-    case 'addData':
-        echo $twig->render('form.html');
-        break;
-    case 'add':
-        addDate();
-        break;
-    case 'delete':
-          delete();
-
-        break;
+    $param = array($twig, '');
+    echo call_user_func_array($go, $param);
 }
-*/
+    else
+        {
+            $param = array($twig, '');
+            $go='index';
+            echo call_user_func_array ($go,$param);
+        }
+
+
+
+
